4c0fc29f1d94a40864958e74402a9d2e49448b79
JerMurphy <jmurphy1@Unb.ca>
2016-11-13 20:08:33 -0400

    Changed the tables

--- app/database/createCoursesTable.sql
+++ app/database/createCoursesTable.sql
@@ -1,7 +1,10 @@
 DROP TABLE IF EXISTS courses
+
 CREATE TABLE courses (
-	id varchar(8) NOT NULL,
-	name varchar(32) NOT NULL,
-	professor varchar(32) NOT NULL,
+	courseID varchar(4) NOT NULL, /* example: 1013 */
+	subject varchar(5) NOT NULL, /* example: CS */
+	course varchar(10) NOT NULL, /* example: CS1013 */ 
+	name varchar(32) NOT NULL, /* example: Intro to Java I */
+	professor varchar(500) NOT NULL, /* example: Natalie Webber, we should try and make this a list somehow */
 	PRIMARY KEY (courseID)
 );
--- app/database/createReviewsTable.sql
+++ app/database/createReviewsTable.sql
@@ -1,10 +1,14 @@
 DROP TABLE IF EXISTS reviews
 
 CREATE TABLE reviews (
-	reviewID int NOT NULL AUTO_INCREMENT,
-	review varchar(500),
-	rating float NOT NULL,
-	courseID varchar(10) NOT NULL,
+	reviewID int NOT NULL AUTO_INCREMENT, /*example: 4 */
+	review varchar(500), /*example: this course sucks */
+	tough_rating float NOT NULL, /*example: 4.87 */
+	courseload_rating float NOT NULL,
+	usefulness_rating float NOT NULL,
+	exam_bool BOOLEAN NOT NULL, /*example: true/false */
+	courseID varchar(10) NOT NULL, /*example: 1013 */
+	postedBy varchar(100) NOT NULL, /*example: jmurphy1 (username) */
 	PRIMARY KEY (reviewID),
 	FOREIGN KEY (courseID) REFERENCES courses(courseID)
 )
