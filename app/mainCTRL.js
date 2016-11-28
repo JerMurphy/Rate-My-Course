95dad41617ef14954ed75d8d95fbc7eb420f2fb2
JLeemur <jon.lee.murray@gmail.com>
2016-11-28 02:30:39 -0400

    so beautiful

--- app/mainCTRL.js
+++ app/mainCTRL.js
@@ -1,54 +1,51 @@
 function mainCTRL($scope,$http) {
    
-    //list the reviews
-    $scope.getReviews = function(id) {
-    	
-     		var url = "https://info3103.cs.unb.ca:43327/reviews/" + id;
+  //list the reviews
+  $scope.getReviews = function(id) {
+ 		var url = "https://info3103.cs.unb.ca:39348/reviews/" + id;
 
-   			$http.get(url).success( function(response) {
-      		$scope.reviews = response;
-   			});
+		$http.get(url).success( function(response) {
+		  $scope.reviews = response;
+		});
+  }
+  //get the CS courses
+  $scope.getCS = function() {
+  	var url = "https://info3103.cs.unb.ca:39348/courses/CS";
 
-	}
-
-    //get the CS courses
-    $scope.getCS = function() {
-    	var url = "https://info3103.cs.unb.ca:43327/courses/CS";
-
-   		$http.get(url).success( function(response) {
-      		$scope.courses = response;
-   		});
+ 		$http.get(url).success( function(response) {
+    	$scope.courses = response;
+ 		});
 	}
 	//get the INFO courses
 	$scope.getINFO = function() {
-    	var url = "https://info3103.cs.unb.ca:43327/courses/INFO";
+  	var url = "https://info3103.cs.unb.ca:39348/courses/INFO";
 
-   		$http.get(url).success( function(response) {
-      		$scope.courses = response;
-   		});
+ 		$http.get(url).success( function(response) {
+    	$scope.courses = response;
+ 		});
 	}
 	//get the MATH courses
 	$scope.getMATH = function() {
-    	var url = "https://info3103.cs.unb.ca:43327/courses/MATH";
+  	var url = "https://info3103.cs.unb.ca:39348/courses/MATH";
 
-   		$http.get(url).success( function(response) {
-      		$scope.courses = response;
-   		});
+ 		$http.get(url).success( function(response) {
+    	$scope.courses = response;
+ 		});
 	}
 	//get the STAT courses
 	$scope.getSTAT = function() {
-    	var url = "https://info3103.cs.unb.ca:43327/courses/STAT";
+  	var url = "https://info3103.cs.unb.ca:39348/courses/STAT";
 
-   		$http.get(url).success( function(response) {
-      		$scope.courses = response;
-   		});
+ 		$http.get(url).success( function(response) {
+    	$scope.courses = response;
+ 		});
 	}
 	//get the MAAC courses
 	$scope.getMAAC = function() {
-    	var url = "https://info3103.cs.unb.ca:43327/courses/MAAC";
+  	var url = "https://info3103.cs.unb.ca:39348/courses/MAAC";
 
-   		$http.get(url).success( function(response) {
-      		$scope.courses = response;
-   		});
+ 		$http.get(url).success( function(response) {
+    	$scope.courses = response;
+ 		});
 	}
 }
